8657b1e87c9e6cd508b276e1ca3ae6558d59fe03
Jacob Staten <jscoding10@protonmail.com>
2026-07-20 20:42:47 -0700

    feat: add models for zillow property info request and response

---
+++ address-validation-and-information-project/Models/ZillowPropertyRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+namespace AddressVerification.Models
+{
+    public class ZillowPropertyRequest
+    {
+        [FromRoute(Name = "address")]
+        [Required]
+        [MinLength(3)]
+        public string Address { get; set; } = string.Empty;
+    }
+}
