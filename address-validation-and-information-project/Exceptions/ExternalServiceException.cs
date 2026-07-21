83d8fb17d14e1735cde6a41ccca1f7073dfd46f9
Jacob Staten <jscoding10@protonmail.com>
2026-07-20 18:10:43 -0700

    feat: add external service exception for errors

---
+++ address-validation-and-information-project/Exceptions/ExternalServiceException.cs
@@ -0,0 +1,9 @@
+﻿namespace AddressVerification.Exceptions;
+
+public class ExternalServiceException : Exception
+{
+    public ExternalServiceException(string message, Exception innerException)
+    : base(message, innerException)
+    {
+    }
+}
