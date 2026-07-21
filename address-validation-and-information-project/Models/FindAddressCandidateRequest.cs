0a57aa275b074a80e8c53b8a0f2f4a09fa03a363
Jacob Staten <jscoding10@protonmail.com>
2026-07-20 18:26:47 -0700

    feat: add models for find address candidate

---
+++ address-validation-and-information-project/Models/FindAddressCandidateRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+namespace AddressVerification.Models
+{
+    public class FindAddressCandidateRequest
+    {
+        [FromRoute(Name = "singleLineAddress")]
+        [Required]
+        public string SingleLineAddress { get; set; } = string.Empty;
+        [FromRoute(Name = "magicKey")]
+        [Required]
+        public string MagicKey { get; set; } = string.Empty;
+        [FromRoute(Name = "countryCode")]
+        [Required]
+        public string CountryCode { get; set; } = string.Empty;
+    }
+}
