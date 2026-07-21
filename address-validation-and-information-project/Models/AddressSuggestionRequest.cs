cd450fcb1583bc4efe42f42c89eeb8e067fc001f
Jacob Staten <jscoding10@protonmail.com>
2026-07-20 18:10:06 -0700

    feat: add models for address suggestion

---
+++ address-validation-and-information-project/Models/AddressSuggestionRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+namespace AddressVerification.Models
+{
+    public class AddressSuggestionRequest
+    {
+        [FromRoute(Name = "address")]
+        [Required]
+        [MinLength(3)]
+        public string Address { get; set; } = string.Empty;
+        [FromRoute(Name = "countryCode")]
+        [Required]
+        [StringLength(2)]
+        public string CountryCode { get; set; } = string.Empty;
+    }
+}
